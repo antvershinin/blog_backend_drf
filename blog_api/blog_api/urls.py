0dbcef24befe1a7e26664f775cbaf2907d868b70
Anton Vershinin <a.vershinin92@gmail.com>
2023-07-04 11:54:51 +0300

    added JWT token authentication

--- blog_api/blog_api/urls.py
+++ blog_api/blog_api/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from api.views import UserDetail, UserList
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', UserList.as_view()),
     path('users/<int:pk>/', UserDetail.as_view()),
     path('api/', include('api.urls')),
